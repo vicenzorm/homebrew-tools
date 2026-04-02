5594f259f0901b901e038305899d6d266b83093d
vicenzorm <vimasera11@gmail.com>
2026-04-02 12:41:35 -0300

    v0.1.1

--- Formula/coge.rb
+++ Formula/coge.rb
@@ -2,7 +2,7 @@ class Coge < Formula
   include Language::Python::Virtualenv
 
   desc "Local AI-powered git commits for the minimalist developer"
-  homepage "https://github.com/vicenzomasera/coge" # Link do repositório original do código
+  homepage "https://github.com/vicenzorm/coge" 
   url "https://github.com/vicenzorm/coge/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "48447249f1603aa1c25e7819350499b4abe09c1ec819304a6a529e6c4ef54e06"
   license "MIT"
@@ -10,7 +10,6 @@ class Coge < Formula
   depends_on "python@3.11"
 
   def install
-    # Isso cria um ambiente virtual Python isolado dentro da pasta do Homebrew
     virtualenv_install_with_resources
   end
 
