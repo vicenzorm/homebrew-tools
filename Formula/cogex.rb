2eb5361f80943d25932b12816100b9721ec75a5f
vicenzorm <vimasera11@gmail.com>
2026-04-03 22:40:17 -0300

    feat(cogex): add local AI-powered git commits for the minimalist developer

---
+++ Formula/cogex.rb
@@ -0,0 +1,17 @@
+class CogeX < Formula
+  desc "Local AI-powered git commits for the minimalist developer"
+  homepage "https://github.com/vicenzorm/CogeX"
+  url "ttps://github.com/vicenzorm/CogeX/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "39e605ea20f9deb1df47051ac4784ebf4affe723f3796d7fe18c27fc360dbcea"
+  license "MIT"
+
+  depends_on "jq"
+
+  def install
+    bin.install "cogex"
+  end
+
+  test do
+    assert_match "cogex v", shell_output("#{bin}/cogex --version")
+  end
+end
